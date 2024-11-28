cb1256775e1bc4c6effb9acf4415733d9ec262dc
kabalera82 <kabalera@gmail.com>
2024-11-28 04:31:10 +0100

    gitnhub enlace

--- components/Footer/Foot.js
+++ components/Footer/Foot.js
@@ -6,9 +6,14 @@ export const AddFooter = () => {
     footer.innerHTML = `
     <p>&copy; 2021</p>
     <ul id="footlist">
-        <li class="foot_list><a href="#">Telegram</a></li>
-        <li class="foot_list><a href="#">Instagram</a></li>
-        <li class="foot_list><a href="#">Facebook</a></li>
+         <li class="foot_list">
+            <a href="https://github.com/kabalera82/Proyecto_4">
+                <img src="/github.png" alt="GitHub" class="foot_img"/>
+            </a>
+        </li>  
+        <li class="foot_list"><a href="#">Telegram</a></li>
+        <li class="foot_list"><a href="#">Instagram</a></li>
+        <li class="foot_list"><a href="#">Facebook</a></li>
     </ul>
     `;
 
